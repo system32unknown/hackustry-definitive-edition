3310ca88177c6129da9d8dbcaa993d27339deb63
Altertoriel <op58274@gmail.com>
2023-03-07 00:03:51 +0900

    aaaaaaa

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,4 @@
-/* 
-    welcome to the start of my spaghetti code
-    have fun trying to read it without dying
-*/
-if(Core.app.isDesktop()) {
-    title();
-}
-
-const menu = require(modName + "/menu");
+const menu = require("menu");
 Events.on(ClientLoadEvent, () => {
     const dialog = menu.setupDialog();
     menu.addSettings(dialog);
